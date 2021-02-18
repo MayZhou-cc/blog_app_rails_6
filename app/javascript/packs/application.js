d2f192e8cd345017a93f1ff6c7df52fc95c21529
Chengyu Zhou <zhouchengyu95@gmail.com>
2021-02-18 22:37:11 +1000

    Cleanup pages layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// This need to be at the bottom
+import "bootstrap"
